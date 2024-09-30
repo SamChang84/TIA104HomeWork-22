279da4db365eccd8f6fa21626e1dcaf697d2068c
Sam <yosamtw@gmail.com>
2024-09-30 17:17:36 +0800

    hw1_1

---
+++ src/hw1/Sum.java
@@ -0,0 +1,10 @@
+package hw1;
+
+public class Sum {
+	public static void main(String[]args) {
+		int x = 6, y = 12;
+		
+		System.out.println("6+12="+(x+y));
+		System.out.println("6*12="+(x*y));
+	}
+}
